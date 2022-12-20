bed478dcd2a674368e51a0384e54a5b4d5ce846e
Liquelaliqour <lewismomanyi34@gmail.com>
2022-12-20 10:55:19 -0800

    quick change

--- Python/8_KYU/Are-You-Playing-Banjo.py
+++ Python/8_KYU/Are-You-Playing-Banjo.py
@@ -15,3 +15,12 @@ def are_you_playing_banjo(name):
     else:
         name += " does not play banjo"
     return name
+
+def areYouPlayingBanjo(name):
+    return name + (' plays' if name[0].lower() == 'r' else ' does not play') + " banjo";
+
+def areYouPlayingBanjo(name):
+    if name.startswith('R') or name.startswith('r'):
+        return name + ' plays banjo'
+    else:
+        return name + ' does not play banjo'
