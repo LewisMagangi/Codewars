1c81d9a80ed48059c3026c2b2454de16bf4b5d8b
Liquelaliqour <lewismomanyi34@gmail.com>
2023-01-02 04:53:31 -0800

    wow

---
+++ C/8_KYU/bool-to-str.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+  /*
+Implement a function which convert the given boolean value into its string representation.
+
+Note: Only valid inputs will be given.
+  */
+  //The returned string should be statically allocated and it won't be freed
+    const char *boolean_to_string(bool b)
+  {
+          if (b == 0)
+                  return "false";
+	  else if (b == 1)
+	          return "true"; // your code here
+  }
+    return 0;
+}
