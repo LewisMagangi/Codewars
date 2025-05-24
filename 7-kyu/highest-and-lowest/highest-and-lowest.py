880499db9d79902d168b8edf17a685d6e5b5055c
Lewis <93725160+LewisMagangi@users.noreply.github.com>
2025-05-24 21:12:58 +0300

    Add solution - CodeHub

---
+++ 7-kyu/highest-and-lowest/highest-and-lowest.py
@@ -0,0 +1,5 @@
+def high_and_low(numbers):
+    numbers_list = [int(num) for num in numbers.split()]
+    sorted_string = sorted(numbers_list)
+    result = f'{sorted_string[-1]} {sorted_string[0]}'
+    return result
